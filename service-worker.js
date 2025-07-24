e4f8e69f05e7fc7307bf88bf74039c5f29caf96d
Sean Ockert <seanockert@gmail.com>
2025-07-24 13:57:32 +1000

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'subs-014';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/bion-bold.woff2',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) return cachedResponse;
+
+        return fetch(event.request.clone())
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => cache.put(event.request, responseToCache));
+
+            return response;
+          })
+          .catch((error) => {
+            if (event.request.mode === 'navigate') {
+              return caches.match('/index.html');
+            }
+            throw error;
+          });
+      })
+  );
+});
